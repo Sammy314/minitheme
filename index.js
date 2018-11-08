d9651a4a79bc6e25760a1c0d5a018fdec34f6738
Sammy <15576256499@163.com>
2018-11-08 08:59:13 +0800

    修改音乐

--- index.js
+++ index.js
@@ -1,17 +1,5 @@
 window.onload = function() {
   
-
- 
-  // document.addEventListener('DOMContentLoaded', function() {
-  //   function audioAutoPlay() {
-  //     musicAudio.play();
-  //     document.addEventListener("WeixinJSBridgeReady", function() {
-  //       musicAudio.play();
-  //     }, false);
-  //   }
-  //   audioAutoPlay();
-  // });
-  
   var year = [ '1978', '1980', '1981', '1984', '1985', '1988', '1989', '1990', '1991', '1991', '1992', '1992', '1992', '1994', '1997', '1999', '2000', '2004', '2005', '2005', '2006', '2006', '2006', '2006', '2007', '2008', '2008', '2008', '2010', '2011', '2012', '2012', '2013', '2013', '2014', '2014', '2015', '2015', '2016', '2016', '2017'];
   var count = 1;
   function circle(){
@@ -142,18 +130,29 @@ window.onload = function() {
     }, 30);
   }
 }
+
+
 function fn(){
   var musicAudio=document.getElementById("musicAudio");
   var music=document.getElementsByClassName("music")[0];
+  document.addEventListener('DOMContentLoaded', function() {
+    function audioAutoPlay() {
+      musicAudio.play();
+      document.addEventListener("WeixinJSBridgeReady", function() {
+        musicAudio.play();
+      }, false);
+    }
+    audioAutoPlay();
+  });
   if(musicAudio.muted){
     musicAudio.muted=false; //静音
     music.style.backgroundImage="url(img/music-on.png)";
     music.style.animation="music 3s linear infinite";
-    // musicAudio.play();
+    musicAudio.paused;
   }else{
     musicAudio.muted=true;
     music.style.backgroundImage="url(img/music-off.png)";
-    music.style.animation="music 0";
-    // musicAudio.paused;
+    music.style.animation="music 0s";
+    musicAudio.play();
   }
 }
