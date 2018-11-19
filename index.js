85dde15f4744b6448736b2e5dd5f083d9cdd616d
Sammy <15576256499@163.com>
2018-11-19 23:28:57 +0800

    把日历又改成了背景图

--- index.js
+++ index.js
@@ -23,11 +23,21 @@
       musicAudio.play();
     }
   }
+// function move(dom, value){
+//   var moveTimer = setInterval(function() {
+//     var top = getStyle(dom, 'top');
+//     top = parseInt(top) -1;
+//     dom.style.top = top + 'px';
+//     if (top == -(value)) {
+//       clearInterval(moveTimer);
+//     }
+//   }, 6);
+// }
 function move(dom, value){
   var moveTimer = setInterval(function() {
-    var top = getStyle(dom, 'top');
+    var top = getStyle(dom, 'backgroundPositionY');
     top = parseInt(top) -1;
-    dom.style.top = top + 'px';
+    dom.style.backgroundPositionY = top + 'px';
     if (top == -(value)) {
       clearInterval(moveTimer);
     }
@@ -52,7 +62,6 @@ $(".button").on("click", function() {
   setTimeout(function() {
     $(".gif").hide();
     $(".canvas-wrap").hide();
-    // $(".page").animate({"opacity": "1"},0);
     $(".page").show();
     var timer = setInterval(circle, 5000);
   }, 3000);
@@ -69,73 +78,73 @@ function circle(){
       break;
     case 2:
     // 80
-      move(stage3, 60);
-      move(stage4, 61);
+      move(stage3, 58);
+      move(stage4, 59);
       break;
     case 3:
     // 81
-      move(stage4, 120);
+      move(stage4, 118);
       break;
     case 4:
     // 84
-      move(stage4, 180);
+      move(stage4, 176);
       break;
     case 5:
     // 85
-      move(stage4, 240);
+      move(stage4, 236);
       break;
     case 6:
     // 1988
-      move(stage4, 300);
+      move(stage4, 296);
       break;
     case 7:
     // 1991
-      move(stage3, 120);
-      move(stage4, 360);
+      move(stage3, 118);
+      move(stage4, 354);
       break;
     case 8:
     // 92
-      move(stage4, 420);
+      move(stage4, 412);
       break;
     case 10:
     // 94
-      move(stage4, 480);
+      move(stage4, 472);
       break;
     case 11:
     // 06
-      move(stage1, 60);
-      move(stage2, 60);
-      move(stage3, 180);
-      move(stage4, 540);
+      move(stage1, 59);
+      move(stage2, 59);
+      move(stage3, 177);
+      move(stage4, 530);
       break;
     case 15:
     // 08
-      move(stage4, 600);
+      move(stage4, 590);
       break;
     case 18:
     // 11
-      move(stage3, 240);
-      move(stage4, 660);
+      move(stage3, 236);
+      move(stage4, 649);
       break;
     case 19:
     // 12
-      move(stage4, 720);
+      move(stage4, 708);
       break;
     case 20:
     // 13
-      move(stage4, 780);
+      move(stage4, 767);
       break;
     case 21:
     // 14
-      move(stage4, 840);
+      move(stage4, 826);
       break;
     case 23:
     // 15
-      move(stage4, 900);
+      move(stage4, 884);
       break;
     case 25:
     // 16
-      move(stage4, 960);
+      move(stage4, 944);
       break;
     case 27:
     // 17
